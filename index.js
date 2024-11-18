e0249c1d6604747c5054f3a9e01f2340830749a1
Shreekant <Shreekant4062@gmail.com>
2024-11-18 16:39:02 +0530

    "Removed path module and updated file paths to use __dirname"

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const path = require("path");
-app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
-  const file = fs.readFileSync("text.txt", "utf8");
+  const file = fs.readFileSync(`${__dirname}/text.txt`, "utf8");
   res.setHeader("Content-Type", "text/html");
 
   res.send(file);
 });
 app.get("/form", (req, res) => {
-  const file = fs.readFileSync("form.txt", "utf8");
+  const file = fs.readFileSync(`${__dirname}/form.txt`, "utf8");
   res.setHeader("Content-Type", "text/html");
 
   res.send(file);
 });
 app.get("/profile", (req, res) => {
   const { name, age, contact, bio } = req.query;
-  const file = fs.readFileSync("profile.txt", "utf8");
+  const file = fs.readFileSync(`${__dirname}/profile.txt`, "utf8");
   let editedFile = file
     .replaceAll("{{name}}", name)
     .replace("{{age}}", age)
