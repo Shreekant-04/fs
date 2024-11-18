f7b80aeb43894f545d99e85f01e3fe38040c1803
Shreekant <Shreekant4062@gmail.com>
2024-11-18 16:33:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/", (req, res) => {
+  const file = fs.readFileSync("text.txt", "utf8");
+  res.setHeader("Content-Type", "text/html");
+
+  res.send(file);
+});
+app.get("/form", (req, res) => {
+  const file = fs.readFileSync("form.txt", "utf8");
+  res.setHeader("Content-Type", "text/html");
+
+  res.send(file);
+});
+app.get("/profile", (req, res) => {
+  const { name, age, contact, bio } = req.query;
+  const file = fs.readFileSync("profile.txt", "utf8");
+  let editedFile = file
+    .replaceAll("{{name}}", name)
+    .replace("{{age}}", age)
+    .replace("{{bio}}", bio);
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.send(editedFile);
+});
+
+let port = 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
